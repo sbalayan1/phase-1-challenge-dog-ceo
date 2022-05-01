1337de25d8c6fe5eab840f51ab924b066475936d
sbalayan1 <balayans2014@yahoo.com>
2022-05-01 05:30:56 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 let dogImageContainer = document.querySelector('#dog-image-container')
 let dogBreedList = document.querySelector('#dog-breeds')
-let renderDogs = (results) => {
-    let images = results[0].message
-    let breeds = Object.keys(results[1].message)
+let dropDown = document.querySelector('#breed-dropdown')
+let breeds;
+let highlightEvent = (e) => {
+   e.target.style['color'] = 'red'
+}
 
+let renderDogImages = (images) => {
     images.forEach((image) => {
         let img = document.createElement('img')
         img.src = image
         dogImageContainer.append(img)
-    })
+    }) 
+}
 
+let renderBreeds = (breeds) => {
     breeds.forEach((breed) => {
         let li = document.createElement('li')
         li.textContent = breed
+        li.addEventListener('click', highlightEvent)
         dogBreedList.append(li)
     })
+}
+
+let renderDogs = (results) => {
+    let images = results[0].message
+    breeds = Object.keys(results[1].message)
+    renderDogImages(images)
+    renderBreeds(breeds)
 }
 
 let fetchDogs = async () => {
@@ -29,6 +42,18 @@ let fetchDogs = async () => {
         console.error(error)
     }
 }
+
+let removeDogs = (dogBreeds) => {
+    while (dogBreeds.lastChild) {
+        dogBreeds.lastChild.remove()
+    }
+}
+
+dropDown.addEventListener('click', (e) => {
+    let filteredBreeds = breeds.filter(breed => breed[0].toLowerCase() === e.target.value)
+    removeDogs(dogBreedList)
+    renderBreeds(filteredBreeds)
+})
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchDogs().then(data => renderDogs(data))
